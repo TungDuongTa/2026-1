16a6d213786887cebb700735f0a03f62fddfa58c
Tung Duong Ta <duongtatung@gmail.com>
2026-01-07 23:16:14 +1100

    fix

--- app/components/ExploreBtn.tsx
+++ app/components/ExploreBtn.tsx
@@ -1,33 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import posthog from "posthog-js";
 
 export default function ExploreBtn() {
   return (
-    <div>
-      <div>
-        <button onClick={() => posthog.capture("test_event")}>
-          Click me for an event
-        </button>
-      </div>
-
-      <button
-        type="button"
-        id="explore-btn"
-        className="mt-7 mx-auto"
-        onClick={() => posthog.capture("my event", { property: "value" })}
-      >
-        <a href="#events">
-          Explore Events
-          <Image
-            src="/icons/arrow-down.svg"
-            alt="arrow-down"
-            width={24}
-            height={24}
-          />
-        </a>
-      </button>
-    </div>
+    <button
+      type="button"
+      id="explore-btn"
+      className="mt-7 mx-auto"
+      onClick={() => console.log("CLICK")}
+    >
+      <a href="#events">
+        Explore Events
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow-down"
+          width={24}
+          height={24}
+        />
+      </a>
+    </button>
   );
 }

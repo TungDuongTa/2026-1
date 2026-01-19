621c70dc0b6bf1af9192cf0494ca11a7105c4ab0
Tung Duong Ta <duongtatung@gmail.com>
2026-01-19 21:42:37 +0700

    fix 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import ExploreBtn from "./components/ExploreBtn";
 import { IEvent } from "@/database";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 // import { events } from "../lib/constants";
+export const dynamic = "force-dynamic";
 export default async function Page() {
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
